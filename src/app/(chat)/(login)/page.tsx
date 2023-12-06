1b3337f2871a0e5bfb74c57f0b4b8b6793073acd
lucasabdouni <lucasabdouni@hotmail.com>
2023-12-06 11:04:43 -0300

    feat: disabled the send button if the sending is already being done and added spin

--- src/app/(chat)/(login)/page.tsx
+++ src/app/(chat)/(login)/page.tsx
@@ -1,22 +1,9 @@
-import { redirect } from 'next/navigation'
 import FormLogin from './components/form-login'
-import { cookies } from 'next/headers'
-
-async function checkTokenExists() {
-  const cookieStore = cookies()
-  const checkCookiesExists = cookieStore.get('chat.token')
-
-  if (checkCookiesExists) {
-    redirect('/home')
-  }
-}
 
 export default async function Home() {
-  await checkTokenExists()
-
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center bg-zinc-100 p-16">
+    <section className="w-full m-auto h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center bg-zinc-100 p-4 sm:p-16">
         <h1 className="text-emerald-900 font-bold text-2xl">Faça o login</h1>
         <FormLogin />
       </div>
--- src/app/(chat)/(login)/register/page.tsx
+++ src/app/(chat)/(login)/register/page.tsx
@@ -2,8 +2,8 @@ import FormLogin from './components/form-register'
 
 export default function Home() {
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center bg-zinc-100 p-16">
+    <section className="w-full m-auto h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center bg-zinc-100 p-4 sm:p-16">
         <h1 className="text-emerald-900 font-bold text-2xl">Criar conta</h1>
         <FormLogin />
       </div>
