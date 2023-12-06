4572beb9dc39a5f267c592e06929a364d76f4344
lucasabdouni <lucasabdouni@hotmail.com>
2023-12-06 13:34:06 -0300

    chore: add client/server env variables boundary

--- src/env.ts
+++ src/env.ts
@@ -1,24 +1,19 @@
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  NEXT_PUBLIC_APP_URL: z.string().url(),
-  NEXT_PUBLIC_SOCKET_SECURY: z.string(),
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_SOCKET_SECURY: z.string(),
+  },
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_SOCKET_SECURY: process.env.NEXT_PUBLIC_SOCKET_SECURY,
+  },
 })
-
-const parsedEnv = envSchema.safeParse({
-  NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-  NEXT_PUBLIC_SOCKET_SECURY: process.env.NEXT_PUBLIC_SOCKET_SECURY,
-})
-
-if (!parsedEnv.success) {
-  console.log(
-    'Invalid environment variables',
-    parsedEnv.error.flatten().fieldErrors,
-  )
-
-  throw new Error('Invalid environment variables')
-}
-
-export const env = parsedEnv.data
